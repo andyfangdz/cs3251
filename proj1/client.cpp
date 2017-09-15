1b693534abc38c8db8c423cf0bdb12aab7054cbd
Andy Fang <andyfang.dz@gmail.com>
2017-09-15 00:24:28 -1000

    Because CLion

--- proj1/client.cpp
+++ proj1/client.cpp
@@ -24,48 +24,48 @@
 
 /* The main function */
 int main(int argc, char *argv[]) {
-  int clientSock;               /* socket descriptor */
-  struct sockaddr_in serv_addr; /* server address structure */
+    int clientSock;               /* socket descriptor */
+    struct sockaddr_in serv_addr; /* server address structure */
 
-  char *accountName;       /* Account Name  */
-  char *servIP;            /* Server IP address  */
-  unsigned short servPort; /* Server Port number */
+    char *accountName;       /* Account Name  */
+    char *servIP;            /* Server IP address  */
+    unsigned short servPort; /* Server Port number */
 
-  char sndBuf[SNDBUFSIZE]; /* Send Buffer */
-  char rcvBuf[RCVBUFSIZE]; /* Receive Buffer */
+    char sndBuf[SNDBUFSIZE]; /* Send Buffer */
+    char rcvBuf[RCVBUFSIZE]; /* Receive Buffer */
 
-  int balance; /* Account balance */
+    int balance; /* Account balance */
 
-  /* Get the Account Name from the command line */
-  if (argc != 4) {
-    printf(
-        "Incorrect number of arguments. The correct format "
-        "is:\n\taccountName serverIP serverPort");
-    exit(1);
-  }
-  accountName = argv[1];
-  memset(&sndBuf, 0, SNDBUFSIZE);
-  memset(&rcvBuf, 0, RCVBUFSIZE);
+    /* Get the Account Name from the command line */
+    if (argc != 4) {
+        printf(
+                "Incorrect number of arguments. The correct format "
+                        "is:\n\taccountName serverIP serverPort");
+        exit(1);
+    }
+    accountName = argv[1];
+    memset(&sndBuf, 0, SNDBUFSIZE);
+    memset(&rcvBuf, 0, RCVBUFSIZE);
 
-  /* Get the addditional parameters from the command line */
-  /*      FILL IN */
+    /* Get the addditional parameters from the command line */
+    /*      FILL IN */
 
-  /* Create a new TCP socket*/
-  /*      FILL IN */
+    /* Create a new TCP socket*/
+    /*      FILL IN */
 
-  /* Construct the server address structure */
-  /*      FILL IN  */
+    /* Construct the server address structure */
+    /*      FILL IN  */
 
-  /* Establish connecction to the server */
-  /*      FILL IN  */
+    /* Establish connecction to the server */
+    /*      FILL IN  */
 
-  /* Send the string to the server */
-  /*      FILL IN  */
+    /* Send the string to the server */
+    /*      FILL IN  */
 
-  /* Receive and print response from the server */
-  /*      FILL IN  */
+    /* Receive and print response from the server */
+    /*      FILL IN  */
 
-  printf("%s\n", accountName);
-  printf("Balance is: %i\n", balance);
-  return 0;
+    printf("%s\n", accountName);
+    printf("Balance is: %i\n", balance);
+    return 0;
 }
--- proj1/server.cpp
+++ proj1/server.cpp
@@ -17,43 +17,51 @@
 #include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
 #include <unistd.h>     /* for close() */
 
+#include <map>
+#include <string>
+
+using namespace std;
+
 #define RCVBUFSIZE 512 /* The receive buffer size */
 #define SNDBUFSIZE 512 /* The send buffer size */
 #define BUFSIZE 40     /* Your name can be as many as 40 chars*/
 
 /* The main function */
 int main(int argc, char *argv[]) {
-  int serverSock;                    /* Server Socket */
-  int clientSock;                    /* Client Socket */
-  struct sockaddr_in changeServAddr; /* Local address */
-  struct sockaddr_in changeClntAddr; /* Client address */
-  unsigned short changeServPort;     /* Server port */
-  unsigned int clntLen;              /* Length of address data struct */
-  char nameBuf[BUFSIZE]; /* Buff to store account name from client */
-  int balance;           /* Place to record account balance result */
+    int serverSock;                    /* Server Socket */
+    int clientSock;                    /* Client Socket */
+    struct sockaddr_in serverAddr; /* Local address */
+    struct sockaddr_in clientAddr; /* Client address */
+    unsigned short serverPort;     /* Server port */
+    unsigned int clientLen;        /* Length of address data struct */
+    char nameBuf[BUFSIZE]; /* Buff to store account name from client */
+    int balance;           /* Place to record account balance result */
 
-  /* Create new TCP Socket for incoming requests*/
-  /*      FILL IN */
+    map<string, int> balanceSheet;
 
-  /* Construct local address structure*/
-  /*      FILL IN */
+    balanceSheet["test"] = 123;
+    /* Create new TCP Socket for incoming requests*/
+    /*      FILL IN */
 
-  /* Bind to local address structure */
-  /*      FILL IN */
+    /* Construct local address structure*/
+    /*      FILL IN */
 
-  /* Listen for incoming connections */
-  /*      FILL IN */
+    /* Bind to local address structure */
+    /*      FILL IN */
 
-  /* Loop server forever*/
-  while (1) {
-    /* Accept incoming connection */
-    /*  FILL IN     */
+    /* Listen for incoming connections */
+    /*      FILL IN */
 
-    /* Extract the account name from the packet, store in nameBuf */
-    /* Look up account balance, store in balance */
-    /*  FILL IN     */
+    /* Loop server forever*/
+    while (1) {
+        /* Accept incoming connection */
+        /*  FILL IN     */
 
-    /* Return account balance to client */
-    /*  FILL IN     */
-  }
+        /* Extract the account name from the packet, store in nameBuf */
+        /* Look up account balance, store in balance */
+        /*  FILL IN     */
+
+        /* Return account balance to client */
+        /*  FILL IN     */
+    }
 }
